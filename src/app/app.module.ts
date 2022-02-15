da795599091cd2c245d390a3c2d488524b202774
徐云金YunjinXu <xuyunjin@foxmail.com>
2022-02-15 11:12:12 +0800

    Wire in data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,58 +1,35 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { TasksState, ArchiveTask, PinTask } from '../state/task.state';
 import { Task } from '../models/task.model';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-task-list',
-    template: `
-   <div class="list-items">
-     <app-task
-       *ngFor="let task of tasksInOrder"
-       [task]="task"
-       (onArchiveTask)="onArchiveTask.emit($event)"
-       (onPinTask)="onPinTask.emit($event)"
-     >
-     </app-task>
-     <div *ngIf="tasksInOrder.length === 0 && !loading" class="wrapper-message">
-       <span class="icon-check"></span>
-       <div class="title-message">You have no tasks</div>
-       <div class="subtitle-message">Sit back and relax</div>
-     </div>
-     <div *ngIf="loading">
-       <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
-         <span class="glow-checkbox"></span>
-         <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
-       </div>
-     </div>
-   </div>
+  selector: 'app-task-list',
+  template: `
+    <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
   `,
 })
 export class TaskListComponent {
+  @Select(TasksState.getAllTasks) tasks$: Observable<Task[]>;
 
-    /**
-    * @ignore
-    * Component property to define ordering of tasks
-    */
-    tasksInOrder: Task[] = [];
+  constructor(private store: Store) {}
 
-    /** Checks if it's in loading state */
-    @Input() loading = false;
+  /**
+   * Component method to trigger the archiveTask event
+   */
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
 
-    /** Event to change the task to pinned */
-    // tslint:disable-next-line: no-output-on-prefix
-    @Output()
-    onPinTask = new EventEmitter<Event>();
-
-    /** Event to change the task to archived */
-    // tslint:disable-next-line: no-output-on-prefix
-    @Output()
-    onArchiveTask = new EventEmitter<Event>();
-
-    @Input()
-    set tasks(arr: Task[]) {
-        this.tasksInOrder = [
-            ...arr.filter(t => t.state === 'TASK_PINNED'),
-            ...arr.filter(t => t.state !== 'TASK_PINNED'),
-        ];
-    }
+  /**
+   * Component method to trigger the pinTask event
+   */
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
+  }
 }
